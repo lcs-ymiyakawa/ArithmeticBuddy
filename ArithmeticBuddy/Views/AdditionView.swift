34aa7ae839d68dc851c7433d87b9ae8b42cf9785
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-31 11:52:41 -0500

    Created the project and finished initial project organization.

---
+++ ArithmeticBuddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  ArithmeticBuddy
+//
+//  Created by 宮川義之助 on 2024/01/31.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
