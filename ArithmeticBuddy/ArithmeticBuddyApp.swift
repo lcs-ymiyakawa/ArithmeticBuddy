12939a3e76b4aaa4e4f3e7ea8845a1cc77b168b1
Yoshi Miyakawa <ymiyakawa@lcs.on.ca>
2024-01-31 11:50:08 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by 宮川義之助 on 2024/01/31.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
